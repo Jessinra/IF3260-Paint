9f48e441f4b4eb0aee821938b6a338ee74b4c2b1
Tony <buddy90_lost@yahoo.co.id>
2019-03-14 09:13:21 +0700

    destroy data structures

--- MoveablePlane.hpp
+++ MoveablePlane.hpp
@@ -11,6 +11,7 @@ protected:
 public:
   MoveablePlane(float x, float y, const vector<Line> &lines, int color, int priority);
   MoveablePlane(float x, float y, const Plane &plane);
+  MoveablePlane();
 
   void selfRotate(float pivotX, float pivotY, float theta) override;
   void selfDilate(float pivotX, float pivotY, float scalingConstant) override;
